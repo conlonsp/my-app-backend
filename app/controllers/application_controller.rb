9afd99c54c5934135f7ccc3721e329f2cf2d60e2
samuelconlon <conlonsp@yahoo.com>
2022-12-19 16:40:23 -0500

    added routes in app_cont

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,37 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
   get "/homes" do
-    # { message: "Good luck with your project!" }.to_json
     homes = Home.all
     homes.to_json
   end
 
+  post "/homes" do
+    home = Home.create(
+      address: params[:address],
+      price: params[:price],
+      square_feet: params[:square_feet]
+    )
+    home.to_json
+  end
+
+  patch "/homes/:id" do
+    home = Home.find(params[:id])
+    binding.pry
+    home.update(
+      address: params[:address],
+      price: params[:price],
+      square_feet: params[:square_feet]
+    )
+    home.to_json
+  end
+
+  delete "/homes/:id" do
+    home = Home.find(params[:id])
+    home.destroy
+    home.to_json
+  end
 end
